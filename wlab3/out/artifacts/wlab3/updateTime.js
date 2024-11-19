bc56c95772320b17faa0f8dac3e13cdc9a7805ae
timur0o31 <almetovtimur2005@gmail.com>
2024-11-19 17:21:52 +0300

    Delete wlab3/out/artifacts/wlab3 directory

--- wlab3/out/artifacts/wlab3/updateTime.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-function updateDateTime(){
-    const now = new Date();
-    const formattedDateTime = now.toLocaleString();
-    document.getElementById('datetime').textContent=formattedDateTime;
-}
-updateDateTime();
-setInterval(updateDateTime,7000);});
